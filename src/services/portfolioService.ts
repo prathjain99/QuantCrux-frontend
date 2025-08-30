c0993f5f8215b3d3de1bb84fc3063d5f236767fa
Pratham <prathjain99@email.com>
2025-08-30 21:11:05 +0530

    connect backend

--- src/services/portfolioService.ts
+++ src/services/portfolioService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://quantcrux-backend.onrender.com/api';
 
 // Types
 export interface Portfolio {
